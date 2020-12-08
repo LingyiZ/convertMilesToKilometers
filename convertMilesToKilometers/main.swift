40f6a1fc9b688e7aa39dc7d03a7408c204c9839a
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-08 00:18:24 -0600

    Initial Commit

---
+++ convertMilesToKilometers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  convertMilesToKilometers
+//
+//  Created by Lingyi Zhan on 12/8/20.
+//  Copyright © 2020 Lingyi Zhan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
